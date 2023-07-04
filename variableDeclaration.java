cf2995589b589149804cdc947bf9537297985c53
Abhishek Mishra <123493687+abhimishra887@users.noreply.github.com>
2023-07-04 22:24:45 +0530

    Create variableDeclaration.java

---
+++ variableDeclaration.java
@@ -0,0 +1,17 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        // Integer variable
+        int number = 10;
+
+        // Double variable
+        double pi = 3.14;
+
+        // String variable
+        String name = "John Doe";
+
+        // Printing the values of the variables
+        System.out.println("Integer number: " + number);
+        System.out.println("Double pi: " + pi);
+        System.out.println("String name: " + name);
+    }
+}
